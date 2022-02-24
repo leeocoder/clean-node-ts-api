acab2d797bbdd8d3c744f4c6348b50cb1f02f4d7
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-02-23 21:35:38 -0300

    test: ensure SignUpController returns 400 if no email is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -24,7 +24,7 @@ describe('SignUp Controller', () => {
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
   })
 })
 
