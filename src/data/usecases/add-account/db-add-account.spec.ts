e3c5e270947dac4b3c059bc09c6088155fbb4ac8
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-03-02 22:07:16 -0300

    refactor group all protocols to a single file

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -1,5 +1,5 @@
 
-import { Encrypter } from '../../protocols/encrypter'
+import { Encrypter } from './db-add-account-protocols'
 import { DbAddAccount } from './db-add-account'
 
 interface sutTypes {
