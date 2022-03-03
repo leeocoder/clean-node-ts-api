1e6e0c42eb3e335bc39063d94da3da1fce7486bf
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-03-02 21:16:43 -0300

    test: ensure EmailValidatorAdapter returns false if Validator returns false

---
+++ src/utils/email-validator-adapter.spec.ts
@@ -0,0 +1,8 @@
+import { EmailValidatorAdapter } from './email-validator-adapter'
+describe('Email Validator Adapter', () => {
+  test('Should return false if validator returns false', () => {
+    const sut = new EmailValidatorAdapter()
+    const isValid = sut.isValid('invalid_email@mail.com')
+    expect(isValid).toBe(false)
+  })
+})
