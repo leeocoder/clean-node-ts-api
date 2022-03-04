eba380efce46650c6a4a207e838f4f1e0c788bae
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-03-03 23:49:49 -0300

    refactor: clear database between tests

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -12,6 +12,11 @@ describe('Account Mongo Repository', () => {
   afterAll(async () => {
     await MongoHelper.disconnect()
   })
+
+  beforeEach(async () => {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
   test('Should return an account on success', async () => {
     const sut = makeSut()
     const account = await sut.add({
