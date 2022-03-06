2c1ded4812673d4ac5c4d36a069d22a6cda79b59
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-03-06 20:39:26 -0300

    test: ensure requests parse body as json

---
+++ src/main/middlewares/body-parser.test.ts
@@ -0,0 +1,16 @@
+import { app } from '../config/app'
+import request from 'supertest'
+
+describe('Body Parser Middlewares', () => {
+  app.post('/test_body_parser', (request, response) => {
+    return response.send(request.body)
+  })
+  test('Should parse body as json', async () => {
+    await request(app)
+      .post('/test_body_parser')
+      .send({ name: 'Leonardo' })
+      .expect({ name: 'Leonardo' })
+  })
+})
+
+request(app)
