6644a47a0af7d5b3da569f23014ea760c992a861
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-03-10 21:08:43 -0300

    feat: ensure LogControllerDecorator returns the same result of the controller

--- src/main/decorators/log.ts
+++ src/main/decorators/log.ts
@@ -7,7 +7,7 @@ export class LogControllerDecorator implements Controller {
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.controller.handle(httpRequest)
-    return await Promise.resolve({ statusCode: 200, body: '' })
+    const httpResponse = await this.controller.handle(httpRequest)
+    return httpResponse
   }
 }
