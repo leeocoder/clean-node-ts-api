f0b732a4285d5fa57aa1c98deff1b0da275b8caf
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-03-11 20:57:56 -0300

    rfefactor: move sut creation to a factory helper method

--- src/infra/db/mongodb/log-repository/log.spec.ts
+++ src/infra/db/mongodb/log-repository/log.spec.ts
@@ -2,6 +2,10 @@ import { Collection } from 'mongodb'
 import { LogMongoRepository } from './log'
 import { MongoHelper } from '../helpers/mongo-helper'
 
+const makeSut = (): LogMongoRepository => {
+  return new LogMongoRepository()
+}
+
 describe('Logo Mongo Repository', () => {
   let errorCollection: Collection
   beforeAll(async () => {
@@ -16,7 +20,7 @@ describe('Logo Mongo Repository', () => {
     await errorCollection.deleteMany({})
   })
   test('Should create an error log on success ', async () => {
-    const sut = new LogMongoRepository()
+    const sut = makeSut()
     await sut.logError('any_error')
     const count = await errorCollection.countDocuments()
     expect(count).toBe(1)
