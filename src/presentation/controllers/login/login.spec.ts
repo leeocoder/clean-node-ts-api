286c50672fe34d0796ac04c00af4ff16ab4e2f89
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-03-12 22:44:26 -0300

    feat: ensure LoginController returns 500 if Authentication throws

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -85,6 +85,12 @@ describe('Login Controller', () => {
     const httpResponse = await sut.handle(makeFakeHttPRequest())
     expect(httpResponse).toEqual(serverError(new Error()))
   })
+  test('Should returns 500 if Authentication throws', async () => {
+    const { sut, authenticationStub } = makeSut()
+    jest.spyOn(authenticationStub, 'auth').mockImplementationOnce(() => { throw new Error() })
+    const httpResponse = await sut.handle(makeFakeHttPRequest())
+    expect(httpResponse).toEqual(serverError(new Error()))
+  })
   test('Should returns 401 if Invalid credential are provided', async () => {
     const { sut, authenticationStub } = makeSut()
     jest.spyOn(authenticationStub, 'auth').mockReturnValueOnce(Promise.resolve(null))
