77b9c42c305327902f41bcb976053c1af442ff92
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-03-17 20:54:02 -0300

    test: ensure AccountMongoRepository returns null if loadByEmail null

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -47,4 +47,9 @@ describe('Account Mongo Repository', () => {
     expect(account?.email).toBe('any_email@mail.com')
     expect(account?.password).toBe('any_password')
   })
+  test('Should return an account on loadByEmail success', async () => {
+    const sut = makeSut()
+    const account = await sut.loadByEmail('any_email@mail.com')
+    expect(account).toBeFalsy()
+  })
 })
