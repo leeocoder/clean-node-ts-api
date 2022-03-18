4408ad46458482ed1b9033dba9a293dd65113fc8
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-03-17 22:44:40 -0300

    refactor: rename SignUp routes with LoginRoutes

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -2,7 +2,7 @@ import { app } from '../config/app'
 import request from 'supertest'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 
-describe('Should return an account on success', () => {
+describe('Login Router', () => {
   beforeAll(async () => {
     return await MongoHelper.connect(process.env.MONGO_URL ?? '')
   })
@@ -14,11 +14,13 @@ describe('Should return an account on success', () => {
     const accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
-  test('Should enable cors', async () => {
-    await request(app)
-      .post('/api/signup')
-      .send({ name: 'Leonardo Albuquerque', email: 'leeodesign@hotmail.com', password: '123456', confirmPassword: '123456' })
-      .expect(200)
+  describe('POST /signup', () => {
+    test('Should return 200 on signup', async () => {
+      await request(app)
+        .post('/api/signup')
+        .send({ name: 'Leonardo Albuquerque', email: 'leeodesign@hotmail.com', password: '123456', confirmPassword: '123456' })
+        .expect(200)
+    })
   })
 })
 
