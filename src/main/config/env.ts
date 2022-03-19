a01d24617e6f463137beb8434d7b4ac22eaeaf4d
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-03-18 21:43:11 -0300

    chore: change mongo uri to a cloud db

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL ?? 'mongodb://localhost:27017/survey-api',
+  mongoUrl: process.env.MONGO_URL ?? 'mongodb+srv://leeodev:U7fEqjbuabeDoufw@survey-clean-api.j3qfw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   port: process.env.PORT ?? 3000,
   jwtSecret: process.env.JWT_SECRET ?? 'eHltDF_+12=4'
 }
