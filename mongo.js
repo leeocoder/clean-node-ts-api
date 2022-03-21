f7d4a9c346f6fc94a926115ec5fa4177fafee7c2
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-03-20 22:37:34 -0300

    chore: remove src/data from git and eslint ignore

--- mongo.js
+++
@@ -1,8 +0,0 @@
-import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper'
-import env from './config/env'
-import { app } from './config/app'
-
-MongoHelper.connect(env.mongoUrl)
-  .finally(() => {
-    app.listen(env.port, () => console.log(`Server is running on port: http://locahost:${env.port}`))
-  }).catch(console.error)
