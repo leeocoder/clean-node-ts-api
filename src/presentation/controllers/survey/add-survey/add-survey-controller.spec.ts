335469df6e7873946c89e72bd283446ebfd498e1
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-03-21 16:42:38 -0300

    refactor: move sut creation to factory method helper

--- src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
@@ -10,15 +10,29 @@ const makeFakeRequest = (): HttpRequest => ({
     }]
   }
 })
+
+const makeValidationStub = (): Validation => {
+  class ValidationStub implements Validation {
+    validate (input: any): Error | null {
+      return null
+    }
+  }
+
+  return new ValidationStub()
+}
+interface sutTypes {
+  sut: AddSurveyController
+  validationStub: Validation
+}
+
+const makeSut = (): sutTypes => {
+  const validationStub = makeValidationStub()
+  const sut = new AddSurveyController(validationStub)
+  return { sut, validationStub }
+}
 describe('AddSurvey Controller', () => {
   test('Should call validation with correct values', async () => {
-    class ValidationStub implements Validation {
-      validate (input: any): Error | null {
-        return null
-      }
-    }
-    const validationStub = new ValidationStub()
-    const sut = new AddSurveyController(validationStub)
+    const { sut, validationStub } = makeSut()
     const validaSpy = jest.spyOn(validationStub, 'validate')
     const httpRequest = makeFakeRequest()
     await sut.handle(httpRequest)
