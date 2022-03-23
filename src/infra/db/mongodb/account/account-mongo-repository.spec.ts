1538be5175e53799143c12db9cc68f600e838db4
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-03-23 09:02:43 -0300

    test: ensure AccountMongoRepostiory will not find an account if findAccountByToken fails

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -103,10 +103,10 @@ describe('Account Mongo Repository', () => {
       expect(account?.email).toBe('any_email@mail.com')
       expect(account?.password).toBe('any_password')
     })
-    // test('Should return null if loadByToken null', async () => {
-    //   const sut = makeSut()
-    //   const account = await sut.loadByToken('any_email@mail.com')
-    //   expect(account).toBeFalsy()
-    // })
+    test('Should return null if loadByToken null', async () => {
+      const sut = makeSut()
+      const account = await sut.loadByToken('any_token')
+      expect(account).toBeFalsy()
+    })
   })
 })
