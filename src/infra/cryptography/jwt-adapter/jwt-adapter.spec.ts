af5ad9a1529cd3a0bac05f1de88cf8bc78e485d7
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-03-23 08:29:04 -0300

    test: ensure JwtAdapter Decrypt returns a value on success

--- src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
@@ -4,6 +4,9 @@ import { JwtAdapter } from './jwt-adapter'
 jest.mock('jsonwebtoken', () => ({
   async sign (): Promise<string> {
     return await Promise.resolve('any_token')
+  },
+  async verify (): Promise<string | null> {
+    return await Promise.resolve('any_value')
   }
 }))
 
@@ -31,6 +34,20 @@ describe('JWT Adapter', () => {
       })
       const promise = sut.encrypt('any_id')
       await expect(promise).rejects.toThrow()
+    })
+  })
+  describe('verify()', () => {
+    test('should call JWT verify with correct values', async () => {
+      const sut = makeSut()
+      const verifySpy = jest.spyOn(jwt, 'verify')
+      await sut.decrypt('any_token')
+      expect(verifySpy).toHaveBeenCalledWith('any_token', 'secret')
+    })
+
+    test('should return a value on sign success', async () => {
+      const sut = makeSut()
+      const value = await sut.decrypt('any_token')
+      expect(value).toBe('any_value')
     })
   })
 })
