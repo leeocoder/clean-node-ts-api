51f02b1ade90059d13d5ca6fe8cd3ff09f9af941
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-03-23 10:09:03 -0300

    test: ensure add sruvey returns 204 if valid accessToken is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -3,8 +3,11 @@ import { app } from '../config/app'
 import request from 'supertest'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import { Collection } from 'mongodb'
+import { sign } from 'jsonwebtoken'
+import env from '../config/env'
 
 let surveyCollection: Collection
+let accountCollection: Collection
 
 describe('Survey Router', () => {
   beforeAll(async () => {
@@ -17,6 +20,9 @@ describe('Survey Router', () => {
   beforeEach(async () => {
     surveyCollection = await MongoHelper.getCollection('surveys')
     await surveyCollection.deleteMany({})
+
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
   })
   describe('POST /surveys', () => {
     test('Should return 403 on add survey without accessToken', async () => {
@@ -29,6 +35,25 @@ describe('Survey Router', () => {
       }
       await request(app)
         .post('/api/surveys')
+        .send(survey)
+        .expect(403)
+    })
+    test('Should return 403 on add survey with valid token', async () => {
+      const accountId = await accountCollection.insertOne({ name: 'Ricardo Albuquerque', email: 'ricalb@mail.com', password: '123', role: 'admin' })
+      const account = await accountCollection.findOne({ _id: accountId.insertedId })
+      console.log('id: ', account?._id)
+      const accessToken = sign({ id: account?._id }, env.jwtSecret)
+      await accountCollection.updateOne({ _id: accountId }, { $set: { accessToken } })
+      const survey: AddSurveyModel = {
+        question: 'any_question',
+        answers: [{
+          image: 'any_image',
+          answer: 'http://imagem.com/this-is-a-image.png'
+        }]
+      }
+      await request(app)
+        .post('/api/surveys')
+        .set('x-access-token', accessToken)
         .send(survey)
         .expect(403)
     })
