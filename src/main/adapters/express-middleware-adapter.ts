946d7b65126c6b84267429bc4503d9291174f0b7
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-03-23 09:39:16 -0300

    feat: ensure AddSurveyRoutes returns 403 if no accessToken is provided

---
+++ src/main/adapters/express-middleware-adapter.ts
@@ -0,0 +1,16 @@
+import { HttpRequest, Middleware } from '../../presentation/protocols'
+import { NextFunction, Request, Response } from 'express'
+
+export const adaptMiddleware = (middleware: Middleware): any => {
+  return async (request: Request, response: Response, next: NextFunction): Promise<Response> => {
+    const httpRequest: HttpRequest = {
+      headers: request.headers
+    }
+    const httpResponse = await middleware.handle(httpRequest)
+    if (Number(httpResponse.statusCode) === 200) {
+      Object.assign(request, httpResponse.body)
+      next()
+    }
+    return response.status(Number(httpResponse.statusCode)).json({ error: httpResponse.body.message })
+  }
+}
