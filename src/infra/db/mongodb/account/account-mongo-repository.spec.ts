47337299c7b87882d9c3db107bd9b4a7740d0412
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-03-23 08:59:31 -0300

    test: ensure AccountMongoRepostiory find an account by token without role

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -71,4 +71,26 @@ describe('Account Mongo Repository', () => {
       expect(account).toBeTruthy()
     })
   })
+  describe('loadByToken()', () => {
+    test('Should return an account on loadByToken without role', async () => {
+      const sut = makeSut()
+      await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        accessToken: 'any_token'
+      })
+      const account = await sut.loadByToken('any_token')
+      expect(account).toBeTruthy()
+      expect(account?.id).toBeTruthy()
+      expect(account?.name).toBe('any_name')
+      expect(account?.email).toBe('any_email@mail.com')
+      expect(account?.password).toBe('any_password')
+    })
+    // test('Should return null if loadByToken null', async () => {
+    //   const sut = makeSut()
+    //   const account = await sut.loadByToken('any_email@mail.com')
+    //   expect(account).toBeFalsy()
+    // })
+  })
 })
