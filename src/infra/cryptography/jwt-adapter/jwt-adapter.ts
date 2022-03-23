d70b1bf005e34ed7fac5c6feddfb4888ff3e192c
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-03-23 08:28:38 -0300

    feat: ensure JwtAdapter Decrypt returns a value on success

--- src/infra/cryptography/jwt-adapter/jwt-adapter.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.ts
@@ -10,8 +10,8 @@ export class JwtAdapter implements Encrypter, Decrypter {
     return accessToken
   }
 
-  async decrypt (value: string): Promise<string | null> {
-    await jwt.verify(value, this.secret)
-    return await Promise.resolve(null)
+  async decrypt (token: string): Promise<string | null> {
+    const value: any = await jwt.verify(token, this.secret)
+    return value
   }
 }
