bde73bbd88e524c018c570f642e6887ce98e8e72
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-03-25 08:59:46 -0300

    feat: ensure LoadSurveys Routes returns 403 if no access troken is provided

---
+++ src/main/factories/controllers/survey/load-surveys/load-surveys-controller-factory.ts
@@ -0,0 +1,8 @@
+import { makeLogControllerDecorator } from '../../../decorators/log-controller-decorator-factory'
+import { LoadSurveysController } from '../../../../../presentation/controllers/survey/load-surveys/load-surveys-controller'
+import { Controller } from '../../../../../presentation/protocols/controller'
+import { makeDbLoadSurveys } from '../../../usecases/survey/load-surveys/db-load-surveys'
+export const makeLoadSurveysController = (): Controller => {
+  const controller = new LoadSurveysController(makeDbLoadSurveys())
+  return makeLogControllerDecorator(controller)
+}
