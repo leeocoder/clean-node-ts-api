191b0520d01cbca06fe7565af99e6d252327d9e8
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-03-26 22:28:21 -0300

    feat: ensure SurveyMongoRepository find Survey By Id on success

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,10 +1,12 @@
+import { ObjectId } from 'mongodb'
+import { LoadSurveyById } from '@/domain/usecases/load-survey-by-id'
 import { MongoHelper } from '../helpers/mongo-helper'
 import { SurveyModel } from '@/domain/models/survey'
 import { AddSurveyModel } from '@/domain/usecases/add-survey'
 import { LoadSurveysRepository } from '@/data/protocols/db/survey/load-surveys-repository'
 import { AddSurveyRepository } from '@/data/usecases/add-survey/db-add-survey-protocols'
 
-export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository {
+export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository, LoadSurveyById {
   async add (surveyData: AddSurveyModel): Promise<void> {
     const surveyCollection = await MongoHelper.getCollection('surveys')
     await surveyCollection.insertOne(surveyData)
@@ -13,14 +15,16 @@ export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRe
   async loadAll (): Promise<SurveyModel[] > {
     const surveyCollection = await MongoHelper.getCollection('surveys')
     const surveysMongo = await surveyCollection.find().toArray()
-    const surveys: SurveyModel[] = surveysMongo.map(survey => {
-      return {
-        id: String(survey._id),
-        question: survey.question,
-        answers: [...survey.answers],
-        created_at: survey.created_at
-      }
+    const surveys: SurveyModel[] = surveysMongo.map((surveyData) => {
+      return MongoHelper.map(surveyData)
     })
     return surveys
+  }
+
+  async loadById (id: string): Promise<SurveyModel> {
+    const surveyCollection = await MongoHelper.getCollection('surveys')
+    const surveyMongo = await surveyCollection.findOne({ _id: new ObjectId(id) })
+    const survey = await MongoHelper.map(surveyMongo)
+    return survey
   }
 }
